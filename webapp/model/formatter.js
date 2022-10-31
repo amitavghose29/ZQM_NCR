5d08abb3e7eabf9291666b9001c10bc05ae8c898
Ankit PATEL <ankit.patel.external@airbus.com>
2022-10-31 13:41:02 +0000

    formatter

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,13 +1,7 @@
 sap.ui.define([], function () {
-<<<<<<< HEAD
-    "use strict";
-    return {
-        prdordDate: function (podate) {
-=======
 	"use strict";
 	return {
 		prdordDate: function (podate) {
->>>>>>> a3e564334fd5ff5a46ded5db82e80a06b27ae92f
             var dispFormat=sap.ui.getCore().getConfiguration().getFormatSettings().getDatePattern("medium");
             if(podate){
                /**var oDateFormat = sap.ui.core.format.DateFormat.getDateInstance({
